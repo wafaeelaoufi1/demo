09efda25b1a2044f0941e0c8da62090db6e501dc
Wafae <you@example.com>
2025-04-29 00:14:15 +0200

    first commit

---
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Application {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World1");
+    }
+
+}
